4a2f518c24d3380947b51974dfe0c2da0515a42a
cruble <cruble4@gmail.com>
2016-01-19 13:53:28 -0500

    fixed typo in send_whole_team

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -235,7 +235,7 @@ class NotificationsController < ApplicationController
         @client.account.messages.create(
                     :from => from,
                     :to => friend.phone,
-                    :body => "From Shufflebot:"
+                    :body => "From Shufflebot: #{message}"
                     )
         end
     
