9dc655001e5bcd0082874da29537e0357d937d28
cruble <cruble4@gmail.com>
2016-02-02 19:31:48 -0500

    adds dependent destroy to game players and bench on game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   belongs_to :season
-  has_many :game_players
-  has_many :game_benches 
+  has_many :game_players, :dependent => :delete_all
+  has_many :game_benches, :dependent => :delete_all 
   has_many :users, through: :game_players
   has_many :users, through: :game_benches
 
